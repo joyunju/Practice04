06ab3065aee2f56693d22290782d1c9bcf762404
joyunju <whdbswn1113@naver.com>
2022-04-20 17:02:34 +0900

    출력 오류 찾기 -> 수정

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -2,5 +2,20 @@
 package com.javaex.practice;
 
 public class Ex01 {
-
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[5];
+		intArray[0] = 3;
+		intArray[1] = 7;
+		intArray[2] = 12;
+		
+		int result = 0;
+		
+		// 오류 이유 --> i<= intArray.length; --> 이렇게 작성 불가
+		
+		for(int i =0; i<intArray.length; i++) {
+			result = result + intArray[i];
+		}
+		System.out.println(result);
+	}
 }
