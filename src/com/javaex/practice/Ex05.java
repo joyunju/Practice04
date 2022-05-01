24c595e965f219967c53f7feb5208af3d9264d2b
joyunju <whdbswn1113@naver.com>
2022-05-01 23:12:11 +0900

    배열

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -6,24 +6,32 @@ import java.util.Scanner;
 
 public class Ex05 {
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("5개의 숫자를 입력해주세요");
-		
-		int[] numbers = new int[5];
 
-		int num = sc.nextInt();
-		
-		double average = 0;
-		
-		for(int i=0; i<numbers.length; i++) {
-			//numbers[i] = put.next();
+		Scanner sc = new Scanner(System.in);
+
+		// System.out.println("5개의 숫자를 입력해주세요");
+
+		int[] numArray = new int[5];
+
+		int num;
+		double sum = 0;
+
+		// 입력받은 숫자를 배열에 저장
+		for (int i = 0; i < numArray.length; i++) {
+			// numbers[i] = put.next();
+			num = sc.nextInt();
+			numArray[i] = num;
 		}
 		
+		// 입력받은 값 더하기
+		for(int i =0; i<numArray.length; i++) {
+			sum = sum + numArray[i];
+		}
 		
-		
-		System.out.println("평균 " + average);
-		
+		//평균 구하기 —> 합 나누기 배열에 저장된 수 
+		//출력값 예시 : 평균은 30.0 입니다.
+		System.out.println("평균은 " + sum / numArray.length + " 입니다.");
+
 		sc.close();
 	}
 
