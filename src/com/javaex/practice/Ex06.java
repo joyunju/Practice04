edfa43e514dbd656269ee11c6fce841cf0e4f73e
joyunju <whdbswn1113@naver.com>
2022-05-02 23:33:11 +0900

    배열

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -1,8 +1,26 @@
+//Ex06 주어진 문자열의 공백을 ‘,’(콤마) 로 변경 후 출력하세요.
+
 package com.javaex.practice;
 
 public class Ex06 {
 	public static void main(String[] args) {
-		
+
+		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l' };
+
+		// 첫번째 문장 출력 : This is a pencil
+		for (int i = 0; i < c.length; i++) {
+			System.out.print(c[i]);
+		}
+		System.out.println("");
+
+		// 공백을 ‘,’(콤마) 로 변경 후 출력
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == ' ') {
+				c[i] = ',';	
+			}
+			System.out.print(c[i]);  // 출력 : This,is,a,pencil
+		}
+
 	}
 
 }
