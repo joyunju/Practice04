d9ec95aa8c91d37416d60e56660d19bde41f8a5c
joyunju <whdbswn1113@naver.com>
2022-05-03 00:08:21 +0900

    주석 추가 :  Math.random()

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -5,6 +5,9 @@ package com.javaex.practice;
 
 public class Ex08 {
 	public static void main(String[] args) {
+		// Math.random() 함수는 double 형으로 0.0이상 1.0 미만 사이의 값을 반환하는 함수
+		// 랜덤함수는 기본형이 Double형이기에 (int) 로 정수로 바꿔주어야 함
+		// 1부터의 값을 뽑고 싶다면 랜덤함수는 0부터 나오기때문에 +1 해주기
 
 		int[] lotto = new int[6];
 
